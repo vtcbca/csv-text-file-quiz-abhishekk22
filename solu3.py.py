5169e258ce4987cb82e73d8fd52244e0cc6a4ea4
abhishekk22 <128130033+abhishekk22@users.noreply.github.com>
2023-08-21 11:13:35 +0530

    Add files via upload

---
+++ solu3.py.py
@@ -0,0 +1,14 @@
+bcainfo=""" BCA course info:
+it is 3 years undergraduate course."""
+with open("BCA.txt", "w") as file:
+     file.write(bcainfo)
+rows={'x':0,'y':0, 'z':0}
+with open('BCA.txt','r') as bca_intro:
+    lines=bca_intro.readlines()
+    for line in lines:
+            f_char=line.strip()[0]
+            if f_char in rows:
+                rows[f_char]+=1
+for char,cout in rows.items():
+    print(f"Number of rows start with '{char}':{cout}")
+                
